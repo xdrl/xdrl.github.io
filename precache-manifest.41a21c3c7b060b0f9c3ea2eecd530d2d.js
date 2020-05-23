1de94fe40794dd3b6ffc5b2c639422519943b259
xdrl <christopher.segerberg@gmail.com>
2020-05-23 23:34:28 +0200

    sass

---
+++ precache-manifest.41a21c3c7b060b0f9c3ea2eecd530d2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b984a485c775b6d054589a7646cb7abd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab7b5817ada21c586a77",
+    "url": "/static/css/main.a1949920.chunk.css"
+  },
+  {
+    "revision": "7f776e24302ca180b0d1",
+    "url": "/static/js/2.78b92e55.chunk.js"
+  },
+  {
+    "revision": "8c73243bcb6696428879640699782bdb",
+    "url": "/static/js/2.78b92e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab7b5817ada21c586a77",
+    "url": "/static/js/main.59347729.chunk.js"
+  },
+  {
+    "revision": "24e8d5a84308de897e3a",
+    "url": "/static/js/runtime-main.c8f0954f.js"
+  }
+]);
